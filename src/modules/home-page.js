25c2df6288004835793fa2a560ea81dad65d7443
Zhu Ye Deng <180813277+d-zhuye@users.noreply.github.com>
2025-02-24 00:41:39 -0800

    Add & Style Footer Social Media & Contact Info

--- src/modules/home-page.js
+++ src/modules/home-page.js
@@ -1,4 +1,4 @@
-(function addMenuEvents() {
+(function controlAnimations() {
   const navbarIcon = document.getElementById("navbar-icon");
   const navbar = document.querySelector(".navbar");
   const overlay = document.getElementById("overlay")
@@ -8,6 +8,12 @@
     navbar.classList.toggle("navbar-visible");
     navbar.classList.toggle("navbar-hidden");
   }) 
+
+  document.getElementById("overlay").addEventListener("click", (e) => {
+    navbar.style.display = "block";
+    navbar.classList.toggle("navbar-visible");
+    navbar.classList.toggle("navbar-hidden");
+  })
 
   navbar.addEventListener("animationstart", (e) => {
     if (e.animationName === "expand") overlay.style.display = "block";
@@ -19,6 +25,14 @@
       navbar.style.display = "none";
       overlay.style.display = "none";
     }
+  })
+
+  const typedOutTexts = document.querySelector(".typed-out");
+  typedOutTexts.addEventListener("animationend", (e) => {
+    console.log(e);
+    if (e.animationName === "blink-caret") {
+      typedOutTexts.style.borderColor = "transparent";
+    } 
   })
 
 })();
